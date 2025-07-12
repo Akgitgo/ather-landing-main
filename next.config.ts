6b0339945924cb838ee14e49fd43cfa38ace8750
akgitgo <kulkarniarya37@gmail.com>
2025-07-12 10:22:08 +0530

    Fix build error: Exclude video segment files from TypeScript compilation

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'standalone',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(m3u8|ts)$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
